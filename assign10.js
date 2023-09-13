e185e9f4cb681c2ae6129bab3d994a7bac92f595
hassanahmedabc <144593676+hassanahmedabc@users.noreply.github.com>
2023-09-13 14:44:33 +0500

    Add files via upload

---
+++ assign10.js
@@ -0,0 +1,88 @@
+//assignment 10 
+// task 10
+    // var scores = [320, 230, 480, 120];
+    // scores.sort();
+    // console.log("Ordered score of students: " + scores);
+//task 11
+    // var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"]
+    // var selectedCities =  cities.slice(0, 3);
+    // console.log(selectedCities);
+//task 12
+    // var arr = ["This", "is", "my", "car"];
+    // console.log("Array: " + arr);
+    // console.log("String: " + arr.join(" "));
+//task 13
+    // var fifoArray = [];
+
+    // fifoArray.push("Value 1");
+    // fifoArray.push("Value 2");
+    // fifoArray.push("Value 3");
+
+    // document.write('Array: ', fifoArray);
+
+    // var firstValue = fifoArray.shift();
+    // var secondValue = fifoArray.shift();
+    // var thirdValue = fifoArray.shift();
+
+    // document.write("<br><br>First value:", firstValue);
+    // document.write("<br>Second value:", secondValue);
+    // document.write("<br>Third value:", thirdValue);
+
+// task 14
+    // var lifoArray = [];
+
+    // lifoArray.push("Value 1");
+    // lifoArray.push("Value 2");
+    // lifoArray.push("Value 3");
+
+    // var lastValue = lifoArray.pop();
+    // var secondLastValue = lifoArray.pop();
+    // var thirdLastValue = lifoArray.pop();
+
+    // console.log("Last value:", lastValue);
+    // console.log("Second last value:", secondLastValue);
+    // console.log("Third last value:", thirdLastValue);
+
+//task 15
+// var phoneManufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+// var dropdownMenu = "<select>" + "<option>" + phoneManufacturers.join("</option><option>") + "</option>" + "</select>";
+
+// document.write(dropdownMenu);
+// assignment 12-13
+// task 05
+// var password = "mypassword";
+    // var userPassword = prompt("Enter Password");
+    // if(userPassword==""){
+    //     alert(" Please enter your password");
+    // }
+    // else if(userPassword==password){
+    //     alert("Correct!");
+    // }
+    // else{
+    //     alert("Incorrect password");
+    // }
+//task 06 
+    // var greeting;
+    // var hour = 13;
+    // if (hour < 18) {
+    // greeting = "Good day";
+    // }
+    // else{
+    // greeting = "Good evening";
+    // }
+//task 07 
+    // var time = +prompt("Enter time in 24 hour format i.e. 1900");
+
+    // if(time>=0000 && time<1200){
+    //     alert("Good Morning!");
+    // }
+    // else if(time>=1200 && time<1700){
+    //     alert("Good Afternoon!");
+    // }
+    // else if(time>=1700 && time<2100){
+    //     alert("Good Evening!");
+    // }
+    // else if(time>=2100 && time<2349){
+    //     alert("Good Night!");
+    // }
