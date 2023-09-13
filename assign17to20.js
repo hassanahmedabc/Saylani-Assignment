265e0a2de86e507b7acddf2b82d8b0154b16a95f
hassanahmedabc <144593676+hassanahmedabc@users.noreply.github.com>
2023-09-13 14:18:44 +0500

    Add files via upload

---
+++ assign17to20.js
@@ -0,0 +1,95 @@
+// assignment 17 to 20
+// task01
+// var array = [[],[],[],[]];
+// task02
+// var matrixArray = [[0, 1, 2, 3], [1, 0 ,1, 2], [2, 1, 0, 1]];
+// document.write(matrixArray[0]+ "</br>" +matrixArray[1]+ "<br>" +matrixArray[2])
+// task03
+// for(var i=0; i<=10; i++ )
+// {
+// document.write(i +"<br>" );
+// }
+// task04
+// var table = +prompt("Enter a number to show its multiplication");
+//     var tableLength = +prompt("Enter length of multiplication");
+//     for(var i = 1; i<=tableLength; i++)
+//     {
+//     document.write(table + "×" + i + " = " + (table*i) + "</br>");
+//      }
+// task05
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"];
+
+//     for(var i=0; i<fruits.length; i++){
+//     document.write('Element index no ', i ,' value ', fruits[i]+"<br>")
+//     }
+// task06a
+// for(var i=1; i<=15; i++ )
+//  {
+// counting=(i);
+// document.write(counting)
+//  }
+// 6b reverse counting
+// for(var i=10; i>=1; i-- )
+//  {
+// counting=(i);
+// document.write(counting)
+//  }
+//  task06 c even
+// for(var i=0; i<=20; i+=2)
+//  {
+// counting=(i);
+// document.write(counting+",")
+//  }
+// task06 d odd
+// for(var i=1; i<=19; i+=2)
+// {
+// counting=(i);
+//  document.write(counting+",")
+//  }
+// task 06
+// for(var i=0; i<=20; i+=2)
+// {
+//  counting=(i);
+//  document.write(counting+"k,")
+// } 
+// task07
+// var bakeryItems  = ["cake", "apple pie", "cookie", "chips", "patties"]
+//     var userSearch = prompt("Welcome to my Bakery, What do you want to order?");
+//     var searchCheck = false;
+//     var itemIndex;
+//     for(var i=0; i<=(bakeryItems.length-1); i++){
+//         if(userSearch==bakeryItems[i]){
+//             searchCheck = true;
+//             itemIndex = i;
+//         }
+//     }
+//     if(searchCheck){
+//         alert(userSearch + " is available at index " + itemIndex + " in our bakery");
+//     }
+//     else{
+//         alert("We are sorry. " + userSearch + " is not available in our bakery");
+//     }
+    // task08
+//  var array = [24, 53, 78, 91, 12];
+//  var largestNum=array[0];   
+//  for(var i=0; i<array.length; i++){
+//  if(array[i]>largestNum){
+//     largestNum=array[i];
+//        }
+//     }
+//     console.log("Array Item: " + array + "\nThe largest number is " + largestNum);
+// task09
+ // var array = [24, 53, 78, 91, 12];
+    // var largestNum=array[0];   
+    // for(var i=0; i<array.length; i++){
+    //     if(array[i]<largestNum){
+    //         largestNum=array[i];
+    //     }
+    // }
+    // console.log("Array Item: " + array + "\nThe smallest number is " + largestNum);
+    // task10
+     // var mulOfFive = "";
+    // for (var i=0; i<=100; i+=5){
+    //     mulOfFive += i + ", ";
+    // }
+    // console.log(mulOfFive);
